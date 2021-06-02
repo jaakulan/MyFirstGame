644a45bc850aa96fe9ca32ce694d2ab2c0ddb5ea
Jaakulan Subeethakumar <jaakulans@hotmail.com>
2021-06-02 11:52:58 -0400

    Add project files.

---
+++ MyFirstGame/win32_platform.cpp
@@ -0,0 +1,39 @@
+#include <Windows.h>
+
+bool running = true;
+
+LRESULT CALLBACK window_callback(_In_ HWND   hwnd, _In_ UINT   uMsg, _In_ WPARAM wParam, _In_ LPARAM lParam) {
+	LRESULT result = 0;
+
+	switch (uMsg) {
+		case WM_CLOSE:
+		case WM_DESTROY: {
+			running = false;
+		} break;
+
+		default: {
+			result = DefWindowProc(hwnd, uMsg, wParam, lParam);
+		}
+	}
+	return result;
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd) {
+	WNDCLASS window_class = {};
+	window_class.style = CS_HREDRAW | CS_VREDRAW;
+	window_class.lpszClassName = "Game Window Class";
+	window_class.lpfnWndProc = window_callback;
+
+	RegisterClass(&window_class);
+
+	HWND window = CreateWindow(window_class.lpszClassName, "Jaak's Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
+
+	while (running) {
+		MSG message;
+		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&message);
+			DispatchMessage(&message);
+		}
+
+	}
+}
