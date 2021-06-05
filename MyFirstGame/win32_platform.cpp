e212233cf01156ff5ed7cedc90d5efdb5baba7d6
Jaakulan Subeethakumar <jaakulans@hotmail.com>
2021-06-05 17:13:21 -0400

    added controls with arrow keys

--- MyFirstGame/win32_platform.cpp
+++ MyFirstGame/win32_platform.cpp
@@ -13,6 +13,7 @@ struct Render_State {
 global_variable Render_State render_state;
 
 #include "renderer.cpp"
+#include "platform_common.cpp"
 
 LRESULT CALLBACK window_callback(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
 	LRESULT result = 0;
@@ -73,14 +74,41 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 	HWND window = CreateWindow(window_class.lpszClassName, "Jaak's Game", WS_OVERLAPPEDWINDOW | WS_VISIBLE, CW_USEDEFAULT, CW_USEDEFAULT, 1280, 720, 0, 0, hInstance, 0);
 	HDC hdc = GetDC(window);
 
+	Input input = {};
+
 	while (running) {
 		MSG message;
+
+		for (int i = 0; i < BUTTON_COUNT; i++) {
+			input.buttons[i].changed = false;
+		}
 		while (PeekMessage(&message, window, 0, 0, PM_REMOVE)) {
-			TranslateMessage(&message);
-			DispatchMessage(&message);
+
+			switch (message.message) {
+			case WM_KEYUP:
+			case WM_KEYDOWN: {
+				u32 vk_code = (u32)message.wParam;
+				bool is_down = ((message.lParam & (1 << 31)) == 0);
+
+				switch (vk_code) {
+				case VK_UP: {
+					input.buttons[BUTTON_UP].is_down = is_down;
+					input.buttons[BUTTON_UP].changed = true;
+				} break;
+				}
+
+			} break;
+
+
+			default: {
+				TranslateMessage(&message);
+				DispatchMessage(&message);
+			}
+			}
 		}
 		clear_screen(0xff5500);
-		draw_rect(0, 0, 1, 1, 0x00ff22);
+		if(input.buttons[BUTTON_UP].is_down)
+			draw_rect(0, 0, 1, 1, 0x00ff22);
 		draw_rect(30, 20, 5, 5, 0x00ff22);
 		draw_rect(-20, 20, 8, 3, 0x00ff22);
 		StretchDIBits(hdc, 0, 0, render_state.width, render_state.height, 0, 0, render_state.width, render_state.height, render_state.memory, &render_state.bitmap_info, DIB_RGB_COLORS, SRCCOPY);
